e44484c798f5c404c53c9fcb28a5c69b46da22ef
sunyk <sunyukun@flipboard.cn>
2020-09-23 11:04:06 +0800

    init

---
+++ src/main/java/me/bengle/leek/LeekLuckApplication.java
@@ -0,0 +1,13 @@
+package me.bengle.leek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeekLuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeekLuckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bengle/leek/LeekLuckApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bengle.leek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeekLuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
